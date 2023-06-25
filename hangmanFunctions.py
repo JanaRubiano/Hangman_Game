8b664cb9eb1a9e50b9f088a328f89b15e7408145
JanaRubiano <124604730+JanaRubiano@users.noreply.github.com>
2023-06-25 14:43:19 -0500

    Add files via upload

---
+++ hangmanFunctions.py
@@ -0,0 +1,38 @@
+from  wonderwords import RandomWord
+import os
+
+# clear screen
+def clr(): 
+    return os.system('cls')
+
+
+# choose level and word
+def level() -> str:
+    r = RandomWord()
+
+    choice = None
+    while choice not in [1, 2, 3]:
+        choice = int(input("Choose \033[32m1\033[0m, \033[33m2\033[0m or \033[91m3\033[0m as level:\n\n\033[32m1. Easy\n\033[33m2. Intermediate\n\033[91m3. Hard\n\033[0m"))
+        clr()
+    match choice:
+        case 1:
+            info = [r.word(word_min_length=8, word_max_length=14), 1]
+        case 2:
+            info = [r.word(word_min_length=5, word_max_length=8), 2]
+        case 3:
+            info = [r.word(word_max_length=5), 3]
+
+    return info
+
+# color the drawings
+def color(num:int, picture:str):
+    match num:
+        case 1:
+            c = "\033[96m{}\033[0m".format(picture)
+            return c
+        case 2:
+            c = "\033[93m{}\033[0m".format(picture)
+            return c
+        case 3:
+            c = "\033[35m{}\033[0m".format(picture)
+            return c
